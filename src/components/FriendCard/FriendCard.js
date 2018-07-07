218872051b42a875bb64196924bd2c220df0d912
unknown <tommyn.gre@gmail.com>
2018-07-06 21:57:38 -0400

    + minecraft chars cards

---
+++ src/components/FriendCard/FriendCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./FriendCard.css";
+
+const FriendCard = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+      </ul>
+    </div>
+    {/* <span className="remove">𝘅</span> */}
+  </div>
+);
+
+export default FriendCard;
